ee876e4b86993fc6f7cb7fb6c23e5ca5fdd98df7
Tim Hollingshead <thollingshead@esri.com>
2016-07-14 09:55:59 -0500

    Add indication of capabilities supported by service

---
+++ src/components/capabilities-indicator.js
@@ -0,0 +1,29 @@
+import {bindable} from 'aurelia-framework';
+
+export class CapabilitiesIndicator {
+	@bindable capabilities = [];
+
+	constructor() {
+		this.supported = new Map([
+			['Get', false],
+			['Put', false],
+			['Add', false],
+			['Remove', false],
+			['Query', false]
+		]);
+	}
+
+	capabilitiesChanged() {
+		this.capabilities = this.capabilities || [];
+
+		const map = this.capabilities.indexOf('Map') > -1;
+		const data = this.capabilities.indexOf('Data') > -1;
+		const query = this.capabilities.indexOf('Query') > -1;
+
+		this.supported.set('Get', query && (!map || data));
+		this.supported.set('Put', this.capabilities.indexOf('Update') > -1);
+		this.supported.set('Add', this.capabilities.indexOf('Create') > -1);
+		this.supported.set('Remove', this.capabilities.indexOf('Delete') > -1);
+		this.supported.set('Query', query && (!map || data));
+	}
+}
