d3b774a0ea8829ced9f18c2fae5fd2382afe085d
Tim Hollingshead <thollingshead@esri.com>
2016-10-15 11:59:10 -0600

    Add idProperty field selector

--- src/components/store-settings.js
+++ src/components/store-settings.js
@@ -1,6 +1,7 @@
 import {bindable} from 'aurelia-templating';
 
 export class StoreSettings {
+	@bindable service = {};
 	@bindable settings = {
 		url: '',
 		flatten: true,
@@ -8,4 +9,13 @@ export class StoreSettings {
 		outFields: [],
 		returnGeometry: false
 	};
+
+	serviceChanged() {
+		if (this.service.objectIdField) {
+			const validIdProperty = this.service.fields.some((field) => field.name === this.settings.idProperty);
+			if (!validIdProperty) {
+				this.settings.idProperty = this.service.objectIdField;
+			}
+		}
+	}
 }
