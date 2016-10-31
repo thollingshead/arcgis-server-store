92eebb51da202aebc5314bb9228fccdae2be1ea4
Tim Hollingshead <thollingshead@esri.com>
2016-10-31 12:13:14 -0600

    Add minified version of the store

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,33 @@
 module.exports = function(grunt) {
 	// Configure Tasks
 	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 		jshint: {
-			src: ['*.js', 'tests/*.js', 'tests/mocking/*.js', '!Gruntfile.js', '!profile.js'],
+			src: ['*.js', 'tests/*.js', 'tests/mocking/*.js', '!Gruntfile.js', '!profile.js', '!ArcGISServerStore.min.js'],
 			options: {
 				jshintrc: '.jshintrc'
 			}
 		},
 
+		uglify: {
+			default: {
+				files: {
+					'ArcGISServerStore.min.js': ['./ArcGISServerStore.js']
+				},
+			},
+			options: {
+				compress: true,
+				mangle: true,
+				preserveComments: false,
+				banner: '/* <%= pkg.name %> <%= pkg.version%> | ' +
+								'<%= grunt.template.today("yyyy-mm-dd") %> | ' +
+								'github.com/thollingshead/arcgis-server-store */\n'
+			}
+		},
+
 		watch: {
 			files: ['*.js'],
-			tasks: ['jshint']
+			tasks: ['jshint', 'uglify']
 		},
 
 		esri_slurp: {
@@ -31,6 +48,7 @@ module.exports = function(grunt) {
 
 	// Load Tasks
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-esri-slurp');
 	grunt.loadNpmTasks('grunt-bower-install-simple');
@@ -40,5 +58,5 @@ module.exports = function(grunt) {
 
 	// User-called Tasks
 	grunt.registerTask('setup', grunt.file.exists('tests/libs/esri/package.json') ? ['bower'] : ['esri_slurp', 'bower']);
-	grunt.registerTask('default', 'Watches the project for changes, and automatically performs linting/testing.', ['jshint', 'watch']);
+	grunt.registerTask('default', 'Watches the project for changes, and automatically performs linting/testing.', ['jshint', 'uglify', 'watch']);
 };
